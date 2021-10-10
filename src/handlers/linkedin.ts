741a9bcfae4fc0eb3975181d7816f0a2ae9fab3e
mrknight21 <86748544+DodobirdWarrior@users.noreply.github.com>
2021-10-10 22:19:03 +1300

    add scraper to handler

--- src/handlers/linkedin.ts
+++ src/handlers/linkedin.ts
@@ -1,9 +1,19 @@
 import 'dotenv/config';
 import { APIGatewayProxyResult, APIGatewayProxyEvent } from 'aws-lambda';
-
+import { LinkedInProfileScraper } from '../index';
 
 export const crawlProfile = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
+
+    const scraper = new LinkedInProfileScraper({
+        sessionCookieValue: `${process.env.LINKEDIN_SESSION_COOKIE_VALUE}`,
+        keepAlive: false
+    });
+
+    await scraper.setup();
+
+    const result = await scraper.run('https://www.linkedin.com/in/jvandenaardweg/');
+
     return {
       statusCode: 200,
       headers: {
@@ -11,12 +21,12 @@ export const crawlProfile = async (event: APIGatewayProxyEvent): Promise<APIGate
         'Access-Control-Allow-*': true,
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify("hello world!!"),
+      body: JSON.stringify(result),
     };
   } catch (err) {
     return {
       statusCode: 500,
-      body: err,
+      body: JSON.stringify(err),
     };
   }
 };
